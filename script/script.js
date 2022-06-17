12be732cf5ff3c5c7c31784e296b634698f63825
RobusV <rverplanke@gmail.com>
2022-06-17 17:14:11 +0200

    make functions work

--- script/script.js
+++ script/script.js
@@ -1,28 +1,33 @@
 
 // Randomly return either ‘rock’, ‘paper’ or ‘scissors’.
 function computerPlay(){
-    const options = ["rock", "paper", "scissors"];
+    const options = ["steen", "papier", "schaar"];
     let computerSelection = Math.floor(Math.random() * 3);
     return options[computerSelection];
 }
 
-// Play a single round of Rock Paper Scissors
+// Play a single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors")|| 
-        (playerSelection == "rock" && computerSelection == "paper")
-        console.log("Verloren");
-    else if (playerSelection == "rock" && computerSelection == "scissors") || 
-        (playerSelection == "paper" && computerSelection == "rock") || 
-        (playerSelection == "rock" && computerSelection == "scissors")
-        console.log("Gewonnen");
-    else 
-        console.log("Gelijkspel");
-    
+    if (
+        playerSelection == "steen" && computerSelection == "papier" || 
+        playerSelection == "papier" && computerSelection == "schaar" || 
+        playerSelection == "steen" && computerSelection == "papier")
+    {
+        console.log(`Verloren! ${computerSelection} verslaat ${playerSelection}.`);
+    }
+    else if (
+        playerSelection == "steen" && computerSelection == "schaar" || 
+        playerSelection == "papier" && computerSelection == "steen" || 
+        playerSelection == "schaar" && computerSelection == "papier")
+    {
+        console.log(`Gewonnen! ${playerSelection} verslaat ${computerSelection}.`);
+    }
+    else{
+        console.log(`Gelijkspel! Beide ${playerSelection}`);
+    }
 }
 
-}
-console.log(computerPlay());
-const playerSelection = "paper";
+const playerSelection = "papier";
 const computerSelection = computerPlay();
+playRound(playerSelection, computerSelection);
 //console.log(playRound(playerSelection, computerSelection));
